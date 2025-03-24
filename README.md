fe5250517f2c77ffa4743a326765a50fc29a8a9d
Екатерина Коврижко <eh1306@yandex.ru>
2025-03-24 17:06:42 +0300

    add brain-even

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 [![Maintainability](https://api.codeclimate.com/v1/badges/e7482f1e42818c23e2cb/maintainability)](https://codeclimate.com/github/kov-ekate/php-project-45/maintainability)
 [![Actions Status](https://github.com/kov-ekate/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/kov-ekate/php-project-45/actions)
+## Демонстрация работы
+[![asciinema](https://asciinema.org/a/jtx0KVZRMgSOI6xpD8TpP4jQF)]
---
+++ src/EvenGame.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BrainGames\EvenGame;
+
+use function cli\line;
+use function cli\prompt;
+
+function isEven($num)
+{
+    if ($num % 2 === 0) {
+        return 'yes';
+    } else {
+        return 'no';
+    }
+}
+
+function playEvenGame()
+{
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    print_r("Answer \"yes\" if the number is even, otherwise answer \"no\".\n");
+    $answers = [];
+    while (count($answers) < 3) {
+        $num = rand();
+        $value = isEven($num);
+        print_r("Question: $num\n");
+        $answer = prompt('Your answer');
+        if ($answer === $value) {
+            print_r("Correct!\n");
+            $answers[] = $answer;
+        } else {
+            print_r("'$answer' is wrong answer ;(. Correct answer was '$value'\n");
+            print_r("Let's try again, $name\n");
+            break;
+        }
+    }
+    if (count($answers) === 3) {
+        print_r("Congratulations, $name!\n");
+    }
+}
