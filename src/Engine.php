fdb524384a3329bbc69c000142a8d3f63624dcfb
Екатерина Коврижко <eh1306@yandex.ru>
2025-03-26 16:31:52 +0300

    fix Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -7,28 +7,25 @@ use function cli\prompt;
 
 function runGame(callable $generateRoundData, string $description): void
 {
-    line("./bin/brain-games");
     line("Welcome to the Brain Game!");
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($description);
-    $answers = [];
+    $numCorrectAnswers = 3;
 
-    while (count($answers) < 3) {
+    for ($i = 0; $i < $numCorrectAnswers; $i++) {
         $roundData = $generateRoundData();
         line("Question: %s", $roundData[0]);
         $answer = prompt('Your answer');
-        if ($answer == $roundData[1]) {
+        $correctAnswer = $roundData[1];
+        if ($answer == $correctAnswer) {
             line("Correct!");
-            $answers[] = $answer;
         } else {
-            line("'$answer' is wrong answer ;(. Correct answer was '$roundData[1]'");
-            line("Let's try again, $name!");
-            break;
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'\nLet's try again, $name!");
+            return;
         }
     }
 
-    if (count($answers) === 3) {
-        line("Congratulations, $name!");
-    }
+    line("Congratulations, $name!");
+
 }
